4ed40de0e0028ab4b40065e6d9b8d760375a44b9
LukasJavorek <l.j.brno@seznam.cz>
2024-04-17 08:40:20 +0200

    read data added

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 def read_data(file_name):
@@ -10,10 +11,25 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as csv_file:
+        content = csv.DictReader(csv_file)
+        data = {}
+        for row in content:
+            for key in row.keys():
+                if key not in data:
+                    data[key] = [int(row[key])]
+                else:
+                    data[key].append(int(row[key]))
 
+        print(data)
+    return data
+
+
+def selection_sort(number_list):
+    
 
 def main():
-    pass
+    read_data("numbers.csv")
 
 
 if __name__ == '__main__':
