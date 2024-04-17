103b433f0c3ca825353f4aec7295dbb85c66fe86
LukasJavorek <l.j.brno@seznam.cz>
2024-04-17 09:24:55 +0200

    direction added

--- sorting.py
+++ sorting.py
@@ -25,13 +25,16 @@ def read_data(file_name):
     return data
 
 
-def selection_sort(number_list):
+def selection_sort(number_list, direction=0):
     idx0 = 0
     for n in range(len(number_list)):
         smallest = number_list[idx0]
         idx = idx0
         for number in number_list[idx0:]:
-            if smallest >= number:
+            if smallest >= number and direction == 0:
+                smallest = number
+                idx_smallest = idx
+            if smallest <= number and direction == 1:
                 smallest = number
                 idx_smallest = idx
             idx = idx + 1
@@ -41,12 +44,11 @@ def selection_sort(number_list):
     return number_list
 
 
-
-
 def main():
+    direction = 0
     data = read_data("numbers.csv")
     for value in data.values():
-        value = selection_sort(value)
+        value = selection_sort(value, direction)
     print(data)
 
 if __name__ == '__main__':
